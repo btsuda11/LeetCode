576d71612780b880e88c21da5f686e86f51c1412
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2022-12-21 16:05:47 -0800

    Time: 68 ms (96.06%), Space: 43.6 MB (17.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,10 +3,11 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
+    const indices = new Map();
     for(let i = 0; i < nums.length; i++) {
-        for(let j = 0; j < nums.length; j++) {
-            if(i < j && nums[i] + nums[j] === target) return [i, j];
-        }
+        const complement = target - nums[i];
+        if (indices.has(complement)) return [indices.get(complement), i];
+        indices.set(nums[i], i);
     }
 };
