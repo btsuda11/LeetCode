c44a22caed7e7c39e8e892eda81749aa049396dc
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2022-12-22 11:35:18 -0800

    Time: 79 ms (89.36%), Space: 43.8 MB (95.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,7 +3,14 @@
  * @return {boolean}
  */
 const isPalindrome = s => {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890".split("");
-    const string = s.split("").filter(char => alphabet.includes(char) || alphabet.includes(char.toLowerCase()));
-    return string.join("").toLowerCase() === string.reverse().join("").toLowerCase();
+    let newStr = s.toLowerCase().replace(/[^0-9a-z]/g, "");
+    let left = 0, right = newStr.length - 1;
+    while (left < right) {
+        if (newStr[left] !== newStr[right]) return false;
+        else {
+            left++;
+            right--;
+        }
+    }
+    return true;
 };
