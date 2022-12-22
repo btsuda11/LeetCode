d9fbaf5e610fb6dd3761cef3af742fae49a33640
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2022-12-21 16:15:29 -0800

    Time: 73 ms (84.74%), Space: 42.5 MB (55.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,10 +4,14 @@
  */
 const isValid = s => {
     const stack = [];
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
     for (let i = 0; i < s.length; i++) {
-        if (stack[stack.length - 1] === '(' && s[i] === ')') stack.pop();
-        else if (stack[stack.length - 1] === '[' && s[i] === ']') stack.pop();
-        else if (stack[stack.length - 1] === '{' && s[i] === '}') stack.pop();
+        if ('([{'.includes(s[i])) stack.push(s[i]);
+        else if (stack[stack.length - 1] === map[s[i]]) stack.pop();
         else stack.push(s[i]);
     }
     return stack.length === 0;
