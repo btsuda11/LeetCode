42fad8fe82b5244a32bc8f26582cd360d2a9c7e8
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-09 17:43:07 -0800

    Time: 77 ms (64.88%), Space: 43.1 MB (38.82%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (numbers, target) => {
+    let left = 0, right = numbers.length - 1;
+    while (left < right) {
+        if (numbers[left] + numbers[right] === target) return [left + 1, right + 1];
+        else if (numbers[left] + numbers[right] > target) right--;
+        else left++;
+    }
+};
