f2d6e5d8b73463c9bdf829764198ae4787f4f03e
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-10 16:03:29 -0800

    Time: 94 ms (85.25%), Space: 44.5 MB (96.90%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,18 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-function lengthOfLongestSubstring(s) {
-    let longest = 0;
-    let counter = new Map();
-    let l = 0;
-    for (var right = 0; right < s.length; ++right) {
-        const cur = s[right];
-        counter.set(cur, (counter.has(cur))? counter.get(cur)+1: 1);
-        while (counter.get(cur) > 1) {
-            counter.set(s[l], counter.get(s[l])-1);
-            ++l;
+const lengthOfLongestSubstring = s => {
+    let left = right = longest = 0;
+    const count = new Map();
+    while (right < s.length) {
+        const curr = s[right];
+        count.set(curr, count.has(curr) ? count.get(curr) + 1 : 1);
+        while (count.get(curr) > 1) {
+            count.set(s[left], count.get(s[left]) - 1);
+            left++;
         }
-        longest = Math.max(longest, right-l+1);
+        longest = Math.max(longest, right - left + 1);
+        right++;
     }
     return longest;
-}
+};
