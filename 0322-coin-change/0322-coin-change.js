54b33e007abbba6bf6f19b3575f2e6af8c85e6e9
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-11 18:44:24 -0800

    Time: 111 ms (90.59%), Space: 45.6 MB (90.64%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+const coinChange = (coins, amount) => {
+    let arr = new Array(amount + 1);
+    arr[0] = 0;
+    for (let i = 1; i < arr.length; i++) {
+        arr[i] = Infinity;
+        coins.forEach(amt => {
+            if (i >= amt) arr[i] = Math.min(arr[i], arr[i - amt]+1);
+        })
+    }
+    return arr[amount] === Infinity ? -1 : arr[amount];
+};
