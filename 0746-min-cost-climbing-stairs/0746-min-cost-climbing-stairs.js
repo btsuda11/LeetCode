c468b781e4a64f1c9b818751925ee69f0a890d66
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-11 17:46:24 -0800

    Time: 68 ms (84.00%), Space: 42.5 MB (80.38%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    const newArr = new Array(cost.length);
+    if (cost.length === 1) return cost[0];
+    if (cost.length === 2) return Math.min(cost[0], cost[1]);
+    newArr[cost.length - 1] = cost[cost.length - 1];
+    newArr[cost.length - 2] = cost[cost.length - 2];
+    for (let i = cost.length - 3; i >= 0; i--) {
+        newArr[i] = Math.min(newArr[i + 2], newArr[i + 1]) + cost[i];
+    }
+    return Math.min(newArr[0], newArr[1]);
+};
