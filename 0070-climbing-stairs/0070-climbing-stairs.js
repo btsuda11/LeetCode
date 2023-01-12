5a5afe8a5a7f4d3ebd55e2121885048aabb30936
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-11 17:25:26 -0800

    Time: 51 ms (98.76%), Space: 42.2 MB (11.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -8,7 +8,7 @@ var climbStairs = function(n) {
     stair_arr[1] = 1;
     stair_arr[2] = 2;
     for(let i = 3; i < n + 2; i++) {
-        stair_arr[i] = stair_arr[i-2]*2 + stair_arr[i-1]-stair_arr[i-2]
+        stair_arr[i] = stair_arr[i-2] + stair_arr[i-1]
     }
     return stair_arr[n];
 };
