9632331ab64c7db732a5bcdc2eaca2faa03a34b1
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-12 18:52:53 -0800

    Time: 87 ms (99.76%), Space: 55.5 MB (35.14%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,24 +3,20 @@
  * @return {number[]}
  */
 const productExceptSelf = nums => {
-    let ans = [];
     
-    let left = new Array(nums.length);
-    let right = new Array(nums.length);
+    let ans = new Array(nums.length);
     
-    left[0] = 1;
-    right[right.length - 1] = 1;
+    ans[0] = 1;
     
-    for (let i = 1; i < left.length; i++) {
-        left[i] = left[i - 1] * nums[i - 1];
+    for (let i = 1; i < ans.length; i++) {
+        ans[i] = ans[i - 1] * nums[i - 1];
     }
     
-    for (let i = left.length - 2; i >= 0; i--) {
-        right[i] = right[i + 1] * nums[i + 1];
-    }
-    
-    for (let i = 0; i < nums.length; i++) {
-        ans.push(left[i] * right[i]);
+    let right = 1;    
+
+    for (let i = ans.length - 1; i >= 0; i--) {
+        ans[i] *= right;
+        right *= nums[i];
     }
     
     return ans;
