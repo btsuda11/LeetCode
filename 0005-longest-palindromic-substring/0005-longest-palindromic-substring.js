fc98d19985ad0fd9b2e155bd29d494dbd5fad5a2
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-14 15:51:52 -0800

    Time: 767 ms (24.77%), Space: 85.4 MB (5.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -4,34 +4,25 @@
  */
 
 
-var longestPalindrome = function(s) {
+const longestPalindrome = s => {
+    const table = [...new Array(s.length + 1)].map(_ => new Array(s.length + 1).fill(false));
+    let longest = '';
     
-	if(s.length <= 1) return s;
-	
-	// construct a 2D array
-    const dp = [...new Array(s.length + 1)].map(_ => new Array(s.length + 1).fill(false));
-	
-    let lps = '';
-    
-	// base case for one character
-    for(let i = 0; i < s.length; i++) {
-        dp[i][i] = true;
-        lps = s[i];
+    for (let i = 0; i < s.length; i++) {
+        table[i][i] = true;
+        longest = s[i];
     }
     
-	// base case for two characters
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === s[i + 1]) dp[i][i+1] = true;
-        if(dp[i][i+1]) lps = s.substring(i, i + 2);
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i + 1]) table[i][i + 1] = true;
+        if (table[i][i + 1]) longest = s.substring(i, i + 2);
     }
-
-    // expand to three or more characters
-    for(let i = s.length - 1; i >= 0; i--) {
-        for(let j = i + 2; j < s.length; j++) {
-            dp[i][j] = dp[i+1][j-1] && s[i] === s[j];
-            if(dp[i][j]) lps = lps.length < (j - i + 1) ? s.substring(i, j + 1) : lps;
+    
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (let j = i + 2; j < s.length; j++) {
+            table[i][j] = table[i + 1][j - 1] && s[i] === s[j];
+            if (table[i][j]) longest = longest.length < (j - i + 1) ? s.substring(i, j + 1) : longest;
         }
     }
-    
-    return lps;
-}
+    return longest;
+};
