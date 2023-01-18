e50be37ee7fe0cea369e345353ef87bfe1079d37
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-18 13:54:08 -0800

    Time: 1343 ms (74.86%), Space: 44.7 MB (86.81%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.js
@@ -0,0 +1,50 @@
+/**
+ * @param {character[][]} board
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findWords = (board, words) => {
+  let res = [];
+
+  const root = buildTrie(words);
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      dfs(root, row, col, board, res);
+    }
+  }
+  return res;
+
+};
+
+dfs = (node, row, col, board, res) => {
+    if (node.end) {
+      res.push(node.end);
+      node.end = null;   // make sure only print one time for each word
+    }
+
+    if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return;
+    if (!node[board[row][col]]) return;
+
+    const c = board[row][col];
+    board[row][col] = '#';  // mark visited
+    dfs(node[c], row + 1, col, board, res); // up
+    dfs(node[c], row - 1, col, board, res); // down
+    dfs(node[c], row, col + 1, board, res); // right
+    dfs(node[c], row, col - 1, board, res); // left
+    board[row][col] = c;  // reset - back track
+}
+
+const buildTrie = words => {
+    const root = {};
+    for (let w of words) {
+      let currNode = root; // here 'currNode' just a reference, that we use to go down from root till last child                              node
+                          // and when we reach last child node - this is the end of the word
+                          // and instead of setting "node.end = true", we set "node.end = word"
+      for (let c of w) {
+        if (!currNode[c]) currNode[c] = {}; // if we already have such node, lets ignore it creating and just                                                  move the currNode
+        currNode = currNode[c];
+      }
+      currNode.end = w; 
+    }
+    return root;
+}
