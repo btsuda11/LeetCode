c45d66a9b022b0f111b9e7e45fa5e61458825915
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-18 13:14:56 -0800

    Time: 68 ms (67.42%), Space: 42.1 MB (53.93%) - LeetHub

---
+++ 0468-validate-ip-address/0468-validate-ip-address.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} queryIP
+ * @return {string}
+ */
+
+const validIPv4 = queryIP => {
+    const tokens = queryIP.split('.');
+    if (tokens.length !== 4) return false;
+    if (!tokens.every(token => /^(0|[1-9]\d*)$/.test(token))) return false;
+    const components = tokens.map(token => parseInt(token, 10));
+    return components.every(component => 0 <= component && component <= 255);
+}
+
+const validIPv6 = (queryIP) => {
+  const tokens = queryIP.split(":");
+  if (tokens.length !== 8) {
+    return false;
+  }
+
+  if (!tokens.every((token) => /^[\da-fA-F]{1,4}$/.test(token))) {
+    return false;
+  }
+
+  return true;
+};
+
+const validIPAddress = queryIP => {
+    if (validIPv4(queryIP)) return 'IPv4';
+    if (validIPv6(queryIP)) return 'IPv6';
+    return 'Neither';
+};
