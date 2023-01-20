ce34df23f865cfbc2e66573675cc9e8115ecaa54
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-19 16:47:34 -0800

    Time: 76 ms (66.95%), Space: 43.8 MB (64.19%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+function reverseBits(n) {
+    let ans = 0;
+    
+    for (let i = 0; i < 32; i++) {
+        ans = ans * 2 + n % 2;
+        n >>>= 1;
+    }
+    
+    return ans;
+}
