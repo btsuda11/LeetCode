ba3b49f26f7bb98d063ea950773fa6c157e31725
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-25 14:58:55 -0800

    Time: 79 ms (73.27%), Space: 46.1 MB (65.30%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canJump = nums => {
+    let lastPos = nums.length - 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        let jump = nums[i];
+        if (jump + i >= lastPos) lastPos = i;
+    }
+    return lastPos === 0;
+};
