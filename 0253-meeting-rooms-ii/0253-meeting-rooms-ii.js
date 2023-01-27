24c75060ebcc7861a19267a421d0434a6e9de3b4
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-27 13:56:41 -0800

    Time: 83 ms (72.11%), Space: 43.6 MB (99.84%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -4,8 +4,13 @@
  */
 
 const minMeetingRooms = intervals => {
-    //sort intervals by start date
+    // sort intervals by start date
     intervals.sort((a,b) => a[0] - b[0]);
+    
+    const getEarliestRoom = r => { // returns the room that ends first
+        r.sort((a,b) => a[1] - b[1]);
+        return r[0]
+    }
 
     let rooms = [intervals[0]];
 
@@ -14,20 +19,15 @@ const minMeetingRooms = intervals => {
         let room = getEarliestRoom(rooms);
         let current = intervals[i];
 
-        //if the room ends before the current interval starts, use the room and update the room end time to current
+        // if the room ends before the current interval starts, use the room and update the room end time to current
         if (room[1] <= current[0]){
             room[1] = current[1]
         }
-        else{ //allocate a room
+        else{ // allocate a room
             rooms.push(current)
         }
 
     }
 
     return rooms.length;
-
-    function getEarliestRoom(r) { //returns the room that ends first
-        r.sort((a,b) => a[1]-b[1])
-        return r[0]
-    }
 };
