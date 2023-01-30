a3ef5be8ecda420fdc97df0c3b038222280cdd9c
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-01-30 15:07:52 -0800

    Time: 79 ms (52.13%), Space: 42.7 MB (98.07%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const majorityElement = nums => {
+    const freq = Math.ceil(nums.length / 2);
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        map[nums[i]] = map[nums[i]] + 1 || 1;
+        if (map[nums[i]] === freq) return nums[i];
+    }
+    console.log(freq)
+};
