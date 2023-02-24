2251805676ab9d0f0d5c430fab44a9d55f1d56ae
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-02-24 15:30:38 -0800

    Time: 94 ms (18.83%), Space: 17.4 MB (14.08%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        def sortBy(interval):
+            return interval[0]
+        def overlap(a, b):
+            return not (a[1] <= b[0] or b[1] <= a[0])
+        intervals.sort(key=sortBy)
+        if len(intervals) == 0:
+            return True
+        interval = intervals[0]
+        right = 1
+        while right < len(intervals):
+            print(interval)
+            second = intervals[right]
+            if overlap(interval, second):
+                return False
+            else:
+                interval = [min(interval[0], second[0]), max(interval[1], second[1])]
+            right += 1
+        return True
+            
+            
