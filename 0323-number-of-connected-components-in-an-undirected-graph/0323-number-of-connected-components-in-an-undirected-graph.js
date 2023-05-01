35cde487e90dac5d5773ed2064e4fe0cd09dd837
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-05-01 13:51:24 -0700

    Time: 72 ms (80.75%), Space: 48 MB (32.75%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
@@ -7,23 +7,23 @@ const countComponents = (n, edges) => {
     const graph = {};
     for (let pair of edges) {
         const [a, b] = pair;
-        if (!(a in graph)) graph[a] = [];
-        if (!(b in graph)) graph[b] = [];
+        if (!graph[a]) graph[a] = [];
+        if (!graph[b]) graph[b] = [];
         graph[a].push(b);
         graph[b].push(a);
     }
+    const visited = new Set();
     let count = 0;
-    const set = new Set();
     const dfs = (graph, node) => {
-        if (set.has(String(node))) return false;
-        set.add(String(node));
+        if (visited.has(String(node))) return false;
+        visited.add(String(node));
         for (let neighbor of graph[node]) {
             dfs(graph, neighbor);
         }
         return true;
     }
     for (let node in graph) {
-        if (set.has(node)) continue;
+        if (visited.has(node)) continue;
         if (dfs(graph, node)) count++;
     }
     return count + (n - Object.keys(graph).length);
