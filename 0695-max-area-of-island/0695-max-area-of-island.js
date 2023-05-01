f89f634ce5e750861978f45dd2bc410c930c82e9
Brendan Tsuda <77601104+btsuda11@users.noreply.github.com>
2023-05-01 13:26:42 -0700

    Time: 81 ms (64.29%), Space: 45.2 MB (63.76%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 const maxAreaOfIsland = grid => {
-    let max = 0;
+    let maxArea = 0;
     const m = grid.length;
     const n = grid[0].length;
-    const sizeIsland = (grid, r, c) => {
-        if (r < 0 || c < 0 || r >= m || c >= n) return 0;
-        if (grid[r][c] === 0) return 0;
-        grid[r][c] = 0;
-        return sizeIsland(grid, r + 1, c) + sizeIsland(grid, r - 1, c) + sizeIsland(grid, r, c + 1) + sizeIsland(grid, r, c - 1) + 1;
+    const sizeOfIsland = (i, j) => {
+        if (i < 0 || j < 0 || i >= m || j >= n) return 0;
+        if (grid[i][j] === 0) return 0;
+        grid[i][j] = 0;
+        return sizeOfIsland(i + 1, j) + sizeOfIsland(i - 1, j) + sizeOfIsland(i, j + 1) + sizeOfIsland(i, j - 1) + 1;
     }
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            let area = sizeIsland(grid, i, j);
-            max = Math.max(max, area);
+            const size = sizeOfIsland(i, j);
+            maxArea = Math.max(maxArea, size);
         }
     }
-    return max;
+    return maxArea;
 };
